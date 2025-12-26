39d81f8068eda559d5d9ad2a8ac1fca9316342f4
Chris Lewis <cmslewis@gmail.com>
2025-12-26 08:09:44 -0800

    fix: reduce short code to 6 chars

--- internal/service/create_service/utils/generate.go
+++ internal/service/create_service/utils/generate.go
@@ -6,7 +6,7 @@ import (
 )
 
 // length is the number of characters each short code should have.
-var length = 8
+var length = 6
 
 // chars contains all characters that can appear in a short code.
 var chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
--- internal/service/create_service/utils/generate_test.go
+++ internal/service/create_service/utils/generate_test.go
@@ -6,7 +6,7 @@ import (
 
 func TestGenerateShortCodeLength(t *testing.T) {
 	result := GenerateShortCode()
-	expectedLength := 8
+	expectedLength := 6
 	if len(result) != expectedLength {
 		t.Errorf("Expected short code length %d, got %d", expectedLength, len(result))
 	}
