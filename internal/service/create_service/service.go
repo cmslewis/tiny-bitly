715dbb4071406e5d0ef833ff42761c5c38de8627
Chris Lewis <cmslewis@gmail.com>
2025-12-26 08:10:49 -0800

    feat: retry until short code is unique

--- internal/service/create_service/service.go
+++ internal/service/create_service/service.go
@@ -2,17 +2,17 @@ package create_service
 
 import (
 	"errors"
+	"fmt"
 	"net/url"
 	"tiny-bitly/internal/dao"
+	errorspkg "tiny-bitly/internal/errors"
 	"tiny-bitly/internal/model"
 	"tiny-bitly/internal/service/create_service/utils"
 )
 
-// The URL of our client-facing service, constituting the first part of every
-// short URL.
-//
-// TODO: Move this to environment variable.
-var serviceUrlHostname = "https://www.tiny-bitly.com/"
+// The maximum number of times to try generating a unique short code before
+// aborting and returning an error.
+var maxTries = 10
 
 // Creates and saves an alias for the provided long URL, then returns the alias.
 func CreateShortURL(dao dao.DAO, originalURL string) (*string, error) {
@@ -22,17 +22,28 @@ func CreateShortURL(dao dao.DAO, originalURL string) (*string, error) {
 		return nil, errors.New("invalid URL")
 	}
 
-	// Create a short code based on the database row ID.
-	shortCode := utils.GenerateShortCode()
+	// Retry until we find a short code not taken yet.
+	var shortCode string
+	numTries := 0
+	for numTries < maxTries {
+		numTries += 1
 
-	// Persist the URL entry.
+		shortCode = utils.GenerateShortCode()
+
+		// Save a new URL record.
 	_, err = dao.URLRecordDAO.Create(model.URLRecord{
 		OriginalURL: *validatedURL,
 		ShortCode:   shortCode,
 	})
-	if err != nil {
+		if errorspkg.IsSystemError(err, errorspkg.SystemErrorShortCodeAlreadyInUse) {
+			// Try again.
+		} else if err != nil {
 		return nil, errors.New("failed to save")
 	}
+	}
+
+	fmt.Printf("Generated a new short code for URL %s: %s", *validatedURL, shortCode)
+
 
 	// Build the short URL.
 	shortURL, err := url.JoinPath(serviceUrlHostname, shortCode)
