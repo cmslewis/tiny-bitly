a546a1eb0be22521872b924cdf342bb94aee97c1
Chris Lewis <cmslewis@gmail.com>
2025-12-26 09:23:47 -0800

    fix: rename param to 'shortCode' in URLRecordDAO

--- internal/dao/daotypes/shortened_url.go
+++ internal/dao/daotypes/shortened_url.go
@@ -4,5 +4,5 @@ import "tiny-bitly/internal/model"
 
 type URLRecordDAO interface {
 	Create(urlRecord model.URLRecord) (*model.URLRecordEntity, error)
-	GetByShortCode(originalURL string) (*model.URLRecordEntity, error)
+	GetByShortCode(shortCode string) (*model.URLRecordEntity, error)
 }
